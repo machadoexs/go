83aeefc36a0810f6c6f29ae2dca47eb5ae7b6de5
Vitor Machado <vitor.machado@stone.com.br>
2024-05-02 19:42:08 -0300

    some types

--- src/cap1-ola/ola.go
+++ src/cap1-ola/ola.go
@@ -3,5 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Olá, Mundo! Com Go :)")
+	bytesnumbers, errors := fmt.Println("Olá, Mundo! Com Go :)")
+	fmt.Println(bytesnumbers, errors)
+
+	x := 10
+	y := "olá mundo"
+
+	fmt.Printf("x: %v, %T\n", x, x)
+	fmt.Printf("y: %v, %T ", y, y)
 }
